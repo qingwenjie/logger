1385a68271636d94aa88cf1c5f2d371bfcd7c76c
qwj <wjqing@landhightech.com>
2019-10-25 17:28:41 +0800

    整理demo

---
+++ demo/logger.go
@@ -0,0 +1,16 @@
+package main
+
+import "logger"
+
+func main() {
+	logger.Text = logger.New(&logger.Options{
+		Level:       logger.DefaultLevel,
+		LogType:     logger.LOG_TYPE_FILE,
+		LogFilePath: "",
+	})
+	t()
+}
+
+func t() {
+	logger.Text.Info("test info")
+}
