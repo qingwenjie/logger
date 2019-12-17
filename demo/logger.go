d49eeaaa5bd695916b933ec884816eea6d884d81
qwj <wjqing@landhightech.com>
2019-12-17 17:42:55 +0800

    logger完善

--- demo/logger.go
+++ demo/logger.go
@@ -6,13 +6,16 @@ func main() {
 	logger.Log = logger.New(&logger.Options{
 		Level:       logger.DefaultLevel,
 		LogFilePath: "",
+		StackDepth:  1,
 	})
 	t()
 }
 
 func t() {
 	logger.Log.Info("Info info")
-	logger.Log.Infof("Info info%s-%d","tttt",111)
+	logger.Log.Infof("Info info%s-%d", "iiii", 111)
+	logger.Log.Warn("Warn info")
+	logger.Log.Warnf("Warn info%s-%d", "wwww", 222)
 	logger.Log.Error("Error info")
 	logger.Log.Debug("Debug info")
 	logger.Log.Trace("Trace info")
